f45ec6ce5b1b1d57831c942819bd6e9391b3478f
Jennifer González <jenniferjacqueline936@gmail.com>
2023-04-22 19:47:49 -0600

    Working with Local Storage

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Header from "./components/Header"; 
 import Formulario from "./components/Formulario";
 import ListadoPacientes from "./components/ListadoPacientes";
@@ -8,7 +8,16 @@ function App() {
   const [clients, setClients] = useState([]);
   const [client, setClient] = useState({});
 
-  const deleteClient = (id) => {
+  useEffect(() => {
+    const clientsLS = JSON.parse(localStorage.getItem("clients")) ?? [];
+    clientsLS.length > 0 && setClients(clientsLS);
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("clients", JSON.stringify(clients));
+  }, [clients]);
+
+  const deleteClient = id => {
     const updatedClients = clients.filter(client => client.id !== id);
     setClients(updatedClients);
   }
